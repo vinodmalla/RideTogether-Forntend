f4aadb101148477fcd981836a78dc36bfc30567a
vinod Malla <mallavinod95@gmail.com>
2026-03-03 19:40:16 +0530

    first commit

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,6 @@
 import {io} from 'socket.io-client';  // Import the io function from the socket.io-client library to establish a connection to the socket server.
 export const connectSocket = () => { // Define a function called connectSocket that will be used to establish a connection to the socket server.
-    return io({url: "https://ridetogether-backend.onrender.com"}); // Use the io function to connect to the socket server at the specified URL (http://localhost:5000) and return the socket instance.
+    return io("https://ridetogether-backend.onrender.com",{
+          transports: ["websocket"],
+    }); // Use the io function to connect to the socket server at the specified URL (http://localhost:5000) and return the socket instance.
 }
